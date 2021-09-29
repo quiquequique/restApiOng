1258358d0ab4ff8595d7cb013319ca49de838a89
Pablo Llanes <pablojllanes@gmail.com>
2021-09-29 09:36:04 -0300

    routes of member added, modify app.js

---
+++ routes/members.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  getAllMembers,
+  getMemberById,
+  createMember,
+  updateMember,
+  deleteMember
+} = require('../controllers/members');
+
+router.get('/', getAllMembers);
+router.get('/:id', getMemberById);
+router.post('/', createMember);
+router.put('/:id', updateMember);
+router.delete('/:id', deleteMember);
+
+module.exports = router;
