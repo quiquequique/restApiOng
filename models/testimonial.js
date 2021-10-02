9627955547b0a6496a9d3cc5fde240ca2952f6f9
Mackenson <fatalisky1@gmail.com>
2021-10-02 17:11:04 -0300

    Testimonials models change relation user-testimonial 1N

--- models/testimonial.js
+++ models/testimonial.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Testimonials.belongsToMany(User, { through: "user_testimonials" });
+      Testimonials.belongsTo(User, { as: "user", foreignKey: "userId" });
     }
   }
   Testimonials.init(
