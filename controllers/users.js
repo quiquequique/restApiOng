039c552a0bbb4f7569489476008d0b799cc0ee3b
dario <tacachojose@gmail.com>
2021-10-05 17:41:25 -0300

    restaurando User

--- controllers/users.js
+++ controllers/users.js
@@ -1,35 +1,84 @@
-const getUsers = (req, res) => {
-	res.send("Get all users");
-};
+'use strict'
 
-const getUserByID = (req, res) => {
-	const { id } = req.params;
-	res.send(`Get user by ID = ${id}`);
-};
+const User = require("../models/user"); //traemos modelo! por que no podemos usar findAll ?? 
+var controller = {
+    all: (req, res) => {
+        User.findAll({
+            attributes: ["firstName","roleId"],
+            }).then((resul) => {
+            res.json(resul);
+            res.status(200);
+            })
+            .catch((err) => {res.json(err);})
 
-const addUser = (req, res) => {
-	const data = req.body;
+    },
+    create : (req, res) => {
+        User.create(
+        {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName, 
+            email: req.body.email, 
+            image: req.body.image, 
+            password: req.body.password, 
+            roleId: req.body.roleId,
+        },
+        {
+            include: "role",
+        }
+        )
+        .then((result) => {
+            res.json(result);
+            res.status(200);
+        })
+        .catch((error) => {
+            res.json(error);
+        });
+    },
+    findById: (req, res) => {
+        User.findByPk(req.params.id)
+        .then((post) => {
+        res.json(post);
+        res.status(200);
+        })
+        .catch((error) => {res.json(error)});
+    },
+    findByName: (req,res)=>{
+        User.findOne({where: {nombre: req.params.nombre}})
+        .then((result) => {
+            res.json(result);
+            res.status(200);
+        })
+        .catch((err) => {res.json(err)})
+    },
+    update: (req, res) => {
+        User.update(
+        {
+            nombre: req.body.name,
+        },
+        {
+            where: {
+            id: req.params.id,
+            },
+        }
+        )
+        .then((result) => {
+        res.json(result);
+        res.status(200);
+        })
+        .catch((err) => {res.json(err)})
+    },
+    delete: (req, res) => {
+        User.destroy({
+        where: {
+            id: req.params.id,
+        },
+        })
+        .then((result) => {
+        res.json(result);
+        res.status(200);
+        })
+        .catch((error) => {res.json(error)})
+    }
+}
 
-	res.json({ msg: "Add new user", data });
-};
-
-const editUser = (req, res) => {
-	const { id } = req.params;
-	const data = req.body;
-
-	res.json({ msg: `Edit user with ID = ${id}`, data });
-};
-
-const deleteUser = (req, res) => {
-	const { id } = req.params;
-
-	res.json({ msg: `Delete user with ID = ${id}` });
-};
-
-module.exports = {
-	getUsers,
-	getUserByID,
-	addUser,
-	editUser,
-	deleteUser,
-};
+module.exports = controller;
