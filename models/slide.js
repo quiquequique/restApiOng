b6c40728dc6f63c7bcf2d62715fa0319d6f10328
dario <tacachojose@gmail.com>
2021-10-08 13:47:43 -0300

    agregamos model migration y seeders de slide.

---
+++ models/slide.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class slide extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  slide.init({
+    imageUrl: DataTypes.STRING,
+    text: DataTypes.STRING,
+    order: DataTypes.INTEGER,
+    organizationId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'slide',
+  });
+  return slide;
+};
