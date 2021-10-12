39f7937d29053ae03a9fd8e52dc26afa47108d25
Pablo Llanes <pablojllanes@gmail.com>
2021-10-12 13:26:45 -0300

    OT82-41 initial commit

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const cors = require("cors");
-require("dotenv").config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const categoriesRouter = require("./routes/categories");
-const activitiesRouter = require("./routes/activities");
-const organizationRouter = require("./routes/organization");
-const membersRouter = require("./routes/activities");
-const newRouter = require("./routes/new");
-const membersRouter = require("./routes/members");
-const imagesRouter = require("./routes/images");
+const indexRouter = require('./routes/index');
+//const usersRouter = require('./routes/users');
+const categoriesRouter = require('./routes/categories');
+const activitiesRouter = require('./routes/activities');
+const organizationRouter = require('./routes/organization');
+const newRouter = require('./routes/new');
+const membersRouter = require('./routes/members');
+const imagesRouter = require('./routes/images');
 
 const app = express();
 app.use(cors());
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/", usersRouter);
-app.use("/organization", organizationRouter);
-app.use("/", membersRouter);
-app.use("/", categoriesRouter);
-app.use("/news", newRouter);
-app.use("/", imagesRouter);
+app.use('/', indexRouter);
+//app.use('/', usersRouter);
+app.use('/organization', organizationRouter);
+app.use('/', membersRouter);
+app.use('/', categoriesRouter);
+app.use('/news', newRouter);
+app.use('/', imagesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -46,11 +45,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
