b7c3a6364f3c0c3713621c8b38b74240a0300506
quiquequique <eabramzon@gmail.com>
2021-10-13 00:03:01 -0300

    email service y formato html funcionando falta integracion con register

--- Services/welcomeEmail.services.js
+++ Services/welcomeEmail.services.js
@@ -1,6 +1,8 @@
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
 
+const { emailHtml, emailSubject } = require( "../helpers/email.helpers")
+
 const sendEmail = () => {
 
 //javascript
@@ -9,9 +11,9 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const msg = {
   to: 'eabramzon@hotmail.com', // Change to your recipient
   from: process.env.SENDGRID_VERIFIED_SENDER, // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  subject: emailSubject,
+  // text: 'and easy to do anywhere, even with Node.js',
+  html: emailHtml,
 }
 sgMail
   .send(msg)
