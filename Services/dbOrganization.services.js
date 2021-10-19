7d2dcceafd34a52b7493001394e5680b5317772f
quiquequique <eabramzon@gmail.com>
2021-10-19 16:23:40 -0300

    refact y patch de organization andando, falta validacion y filtro administrador

---
+++ Services/dbOrganization.services.js
@@ -0,0 +1,43 @@
+const { Organization } = require('../models');
+
+const getOrganization = async () => {
+  const organization = await Organization.findAll({ attributes: ['name', 'image', 'phone', 'address'] });
+
+  const organizations = { meta: {
+    status: '200',
+    link: '/organization/public',
+    count: organization.length
+  },
+  data: organization
+  // return organizations;
+  };
+
+  return organizations;
+};
+
+const checkIfExistOrg = async (id) => {
+  const exist = await Organization.findByPk(id);
+  return !!exist;
+};
+
+
+const updateOrganization = async (body, orgId) => {
+  try {
+    const exist = await checkIfExistOrg(orgId);
+    if (exist) {
+      const updated = await Organization.update(body, { where: { id: orgId } });
+      if (updated[0] === 1) {
+        return { msg: { updated: true } };
+      }
+    }
+    return null;
+  } catch (error) {
+    return error;
+  }
+};
+
+
+module.exports = {
+  getOrganization,
+  updateOrganization
+};
--- routes/organization.routes.js
+++ routes/organization.routes.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var router = express.Router();
-const organizationController = require('../controllers/organizationController.controller');
+const express = require('express');
+
+const router = express.Router();
+
+const organizationController = require('../controllers/organization.controller');
 
 /* GET users listing. */
-router.get('/public', organizationController.getAll);
+router.get('/public', organizationController.getOne);
+router.patch('/:id', organizationController.patchOne);
 
 module.exports = router;
