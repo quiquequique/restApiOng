3036f763f83b37870b04736f684070a9e7411ce0
quiquequique <eabramzon@gmail.com>
2021-10-23 17:39:03 -0300

    rout /backoffice/contacts working only for auth. admin user

--- routes/contacts.routes.js
+++ routes/contacts.routes.js
@@ -2,11 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 
-const { createContact } = require('../controllers/contacts.controller');
+const { createContact, getAllContacts } = require('../controllers/contacts.controller');
 
 const { contactsValidator } = require('../middlewares/contacts.validator');
 
+const { isAdmin } = require('../middlewares/isAdmin');
+
+const { isAuthenticated } = require('../middlewares/isAuthenticated');
+
 
 router.post('/', contactsValidator, createContact);
+router.get('/', [isAuthenticated, isAdmin], getAllContacts);
 
 module.exports = router;
