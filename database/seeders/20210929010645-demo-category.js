50b493abdda553ae7ddcfba1fb62ff31db60d1b9
quiquequique <eabramzon@gmail.com>
2021-10-27 19:17:37 -0300

    category pagination added and working

--- database/seeders/20210929010645-demo-category.js
+++ database/seeders/20210929010645-demo-category.js
@@ -6,7 +6,7 @@ module.exports = {
       'Categories',
       [
         {
-          name: 'category one',
+          name: 'category 1',
           description: 'this is the category one',
           image:
             'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
@@ -14,7 +14,327 @@ module.exports = {
           updatedAt: new Date()
         },
         {
-          name: 'category two',
+          name: 'category 2',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 3',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 4',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 5',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 6',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 7',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 8',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 9',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 10',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 11',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 12',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 13',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 14',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 15',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 16',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 17',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 18',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 19',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 20',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 21',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 22',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 23',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 24',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 25',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 26',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 27',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 28',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 29',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 30',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 31',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 32',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 33',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 34',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 35',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 36',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 37',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 38',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 39',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 40',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 41',
+          description: 'this is the category two',
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'category 42',
           description: 'this is the category two',
           image:
             'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
