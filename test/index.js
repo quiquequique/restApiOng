d7fb0cdaaaf9be490ff13345128567a2a2ef6a18
nitovillafuerte <nitovillafuerte@outlook.com>
2021-11-02 18:22:10 -0300

    Añadi los test del endpoint news

---
+++ test/index.js
@@ -0,0 +1,11 @@
+const news = {
+  name: "Carlos",
+  content: "Carla",
+  image: "marla12345@gmail.com",
+};
+const newsedit = {
+  name: "Carlosedit",
+  content: "Carlaedit",
+  image: "marla12345edit@gmail.com",
+};
+module.exports = { news, newsedit };
